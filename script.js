841299a0453d424ddadc455f8ce4b032d9ab48ec
louisbrillet <louis.brillet@gmail.com>
2022-05-03 21:56:00 +0200

    feat: add basics

---
+++ script.js
@@ -0,0 +1,16 @@
+// function addCss(fileName) {
+
+//     var head = document.head;
+//     var link = document.createElement("link");
+  
+//     link.type = "text/css";
+//     link.rel = "stylesheet";
+//     link.href = fileName;
+  
+//     head.appendChild(link);
+//   }
+  
+//   addCss('iframe-style.css');
+//   console.log("script TEEEEST")
+
+  WA.chat.sendChatMessage('Hello world', 'Mr Robot');
